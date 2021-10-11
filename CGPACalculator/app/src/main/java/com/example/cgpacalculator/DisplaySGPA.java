56a06733ce41dafcaa22aa8e9b6d4797cd937273
Swetha Magesh <t-smagesh@microsoft.com>
2021-10-11 12:37:06 +0530

    create activities

---
+++ CGPACalculator/app/src/main/java/com/example/cgpacalculator/DisplaySGPA.java
@@ -0,0 +1,14 @@
+package com.example.cgpacalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplaySGPA extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_sgpa);
+    }
+}
---
+++ CGPACalculator/app/src/main/java/com/example/cgpacalculator/SelectSemester.java
@@ -0,0 +1,14 @@
+package com.example.cgpacalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectSemester extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_semester);
+    }
+}
